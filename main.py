7f792a34d8e4cf928355c09d5cd1a7de04c1128f
kaldykristof <kaldykristof@gmail.com>
2019-11-03 12:10:42 +0100

    find possible plate rois

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 img_original = cv2.imread("./images/img_1.jpg")
@@ -5,6 +6,18 @@ img_grayscale = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
 img_contrast = cv2.convertScaleAbs(img_grayscale, alpha = 1.25, beta = 0)
 ret, img_threshold = cv2.threshold(img_contrast, 0, 255, cv2.THRESH_OTSU)
 
-cv2.imshow("Treshold", img_threshold)
+contours, hierarchy = cv2.findContours(img_threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+possiblePlates = []
+
+for contour in contours:
+    (x, y, width, height) = cv2.boundingRect(contour)
+    area = width * height
+    roi = img_threshold[y:y+height, x:x+width]
+    if (2000 < area < 6000) and (width >= 2 * height):
+        possiblePlates.append(roi)
+        cv2.rectangle(img_original, (x,y), (x+width,y+height), (0,255,0), 2)
+
+cv2.imshow("Possible plates", img_original)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
