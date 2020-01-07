11ece55f813947974b6633cb688079fa6767cc71
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-01-07 12:19:52 +0100

    vizsga módosítás

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from glob import glob
 from os import path
 import timeit
 
+# True  => manuálisan kell lépkedni a képek között, a végén % összegzés
+# False => automatikus, nem mutatja a képeket csak a végén a %-ot
+STEP_BY_STEP = False
+
+full_match = 0  # Hány képen ismerte fel az összes karaktert
+half_match = 0  # Hány képen ismerte fel legalább a karakterek felét
+no_match = 0    # Hány képen nem ismert fel egy karaktert sem
+
+characters_not_found = [] # Karakterek, amiket nem ismert fel az egyes képeken
+
 # Template karakterek betöltése
-characters = []
-glob_characters = glob("characters/*.png")
-for char in glob_characters:
-    characters.append(char)
+characters = glob("characters/*.png")
 
 # Tesztképek betöltése
-images = []
-glob_images = glob("images/*.jpg")
-for img in glob_images:
-    images.append(img)
+images = glob("images/*.jpg")
 
-for image in images:
+def secondElement(element): 
+        return element[1]
+
+for i, image in enumerate(images):
     start_time = timeit.default_timer()
 
     # Bemeneti kép feldolgozása
@@ -71,33 +78,65 @@ for image in images:
                 img_license_plate = possible_plate
                 cv2.rectangle(img_original, (x,y), (x+width,y+height), (0,255,0), 2)
 
+    # Várt rendszám
     expected_plate = str(path.basename(image)).split(".")[0]
-    print("\nVárt rendszám:\n{0}".format(expected_plate))
 
     # Karakterek sorba rendezése oszlop alapján
-    def sortBySecond(element): 
-        return element[1]
-    found_characters.sort(key = sortBySecond)  
+    found_characters.sort(key = secondElement)
 
-    # Formázott rendszám kiíratása
-    print("\nTalált rendszám:")
+    # Karakterek stringé konvertálása
     license_plate = ""
     for character in found_characters:
         license_plate += character[0]
-    print(license_plate)
 
-    print("\nRendszám sikeresen felismerve!" if expected_plate == license_plate else "\nA rendszám felismerése sikertelen!")
-
-    if (len(found_characters) > 0):
-        cv2.imshow("Eredeti kep", img_original)
-        cv2.imshow("Talalt rendszam", img_license_plate)
-    else:
-        print("\nNem található rendszám!")
+    if (len(license_plate) == 7):
+        full_match += 1
+    if (len(license_plate) > 3):
+        half_match += 1
+    if (len(license_plate) == 0):
+        no_match += 1
 
     # Számítási idő kiszámítása
     stop_time = timeit.default_timer()
-    runtime = round(stop_time - start_time, 5)
-    print("\nSzámítási idő: {0} másodperc".format(runtime))
+    runtime = round(stop_time - start_time, 2)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Fel nem ismert karakter helyére '_' karaktert rak
+    formatted_plate = ""
+    for i, found_char in enumerate(license_plate):
+        if (found_char == expected_plate[i]):
+            formatted_plate += found_char
+        else:
+            formatted_plate += '_' + found_char
+    while (len(formatted_plate) != 7):
+        formatted_plate += '_'
+
+    if (STEP_BY_STEP):
+        print("Várt rendszám:   {0}".format(expected_plate))
+        print("Talált rendszám: {0}".format(formatted_plate, runtime))
+        print("Rendszám sikeresen felismerve!" if (expected_plate == license_plate) else "A rendszám felismerése sikertelen!")
+        print("Számítási idő: {0} másodperc".format(runtime))
+        print("-------------------------------------------------")
+
+        if (len(found_characters) > 0):
+            cv2.imshow("Eredeti kep", img_original)
+            cv2.imshow("Talalt rendszam", img_license_plate)
+        else:
+            print("\nNem található rendszám!")
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    # Fel nem ismert karakterek szűrése
+    for i, exp_char in enumerate(expected_plate):
+        if (exp_char != formatted_plate[i] and exp_char not in characters_not_found):
+            characters_not_found.append(exp_char)
+
+print("-------------------------------------------------")
+print("Összes kép száma: {0} db\nEbből:".format(len(images)))
+print(" - 100 %-os találat: {0} db ({1}%)".format(full_match, round(full_match / len(images) * 100, 2)))
+print(" - >50 %-os találat: {0} db ({1}%)".format(half_match, round(half_match / len(images) * 100, 2)))
+print(" - <50 %-os találat: {0} db ({1}%)".format(len(images)-half_match, round((len(images)-half_match) / len(images) * 100, 2)))
+print(" -   0 %-os találat: {0} db ({1}%)".format(no_match, round(no_match / len(images) * 100, 2)))
+print("Fel nem ismert karakterek:")
+print(*characters_not_found, sep = ", ")  
+print("-------------------------------------------------")
